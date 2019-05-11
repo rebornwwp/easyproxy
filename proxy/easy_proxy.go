b9b71e3783f68288cbfabd61fe333a14dcb85e59
wanpeng <rebornwwp@gmail.com>
2019-05-11 23:12:18 +0800

    static record

--- proxy/easy_proxy.go
+++ proxy/easy_proxy.go
@@ -1,12 +1,27 @@
 package proxy
 
-import "github.com/rebornwwp/easyproxy/proxy/schedule"
+import (
+	"github.com/rebornwwp/easyproxy/config"
+	"github.com/rebornwwp/easyproxy/proxy/schedule"
+)
 
 const (
 	DefaultTimeOut = 3
 )
 
 type EasyProxy struct {
-	proxyData *ProxyData
-	strategy  schedule.Strategy
+	data     *ProxyData
+	strategy schedule.Strategy
+}
+
+func (proxy *EasyProxy) Init(config *config.Config) {
+	proxy.data = new(ProxyData)
+	proxy.data.Init(config)
+	proxy.setStrategy(config.Strategy)
+	InitStatistic(proxy.data)
+}
+
+func (proxy *EasyProxy) setStrategy(name string) {
+	proxy.strategy = schedule.GetStrategy(name)
+	proxy.strategy.Init()
 }
